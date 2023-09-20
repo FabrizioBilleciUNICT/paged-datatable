768529d4e63b21bd0ae0ef7bfce74163bcd112a2
fabrizio <fabriziobilleci7@gmail.com>
2023-09-20 16:31:03 +0200

    remove dropdownformfield

--- lib/src/paged_datatable_footer.dart
+++ lib/src/paged_datatable_footer.dart
@@ -55,27 +55,33 @@ class _PagedDataTableFooter<TKey extends Object, TResult extends Object>
                             const SizedBox(width: 10),
                             ConstrainedBox(
                               constraints: const BoxConstraints(maxWidth: 100),
-                              child: DropdownButtonFormField<int>(
-                                  value: theme.configuration.initialPageSize,
-                                  decoration: const InputDecoration(
-                                      isCollapsed: true,
-                                      contentPadding: EdgeInsets.symmetric(
-                                          horizontal: 6, vertical: 8),
-                                      border: OutlineInputBorder()),
-                                  style: const TextStyle(fontSize: 14),
-                                  onChanged:
-                                      state.tableState == _TableState.loading
-                                          ? null
-                                          : (newPageSize) {
-                                              if (newPageSize != null) {
-                                                state.setPageSize(newPageSize);
-                                              }
-                                            },
-                                  items: theme.configuration.pageSizes!
-                                      .map((e) => DropdownMenuItem(
-                                          value: e, child: Text(e.toString())))
-                                      .toList()),
-                            )
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    border: Border.all(
+                                      color: Colors.grey.withOpacity(0.2),
+                                      width: 1,
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 8),
+                                    child: DropdownButton<int>(
+                                    value: theme.configuration.initialPageSize,
+                                    style: const TextStyle(fontSize: 14),
+                                    onChanged:
+                                        state.tableState == _TableState.loading
+                                            ? null
+                                            : (newPageSize) {
+                                                if (newPageSize != null) {
+                                                  state.setPageSize(newPageSize);
+                                                }
+                                              },
+                                    items: theme.configuration.pageSizes!
+                                        .map((e) => DropdownMenuItem(
+                                            value: e, child: Text(e.toString())))
+                                        .toList()),
+                                  )
+                            ))
                           ],
                         ),
                         Padding(
